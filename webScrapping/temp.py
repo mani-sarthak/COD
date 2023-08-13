7822480260e6796d5e4ad9c43609d15d24f4f0b5
mani-sarthak <manisarthakiitd@gmail.com>
2023-08-13 22:17:54 +0530

    taking inputs from all states

--- webScrapping/temp.py
+++ webScrapping/temp.py
@@ -1,14 +1,6 @@
 import requests
 
 
-response = requests.post("https://arc.indiawris.gov.in/server/rest/services/Common/Administrative_SOI/MapServer/1/query?f=json&orderByFields=district&outFields=*&spatialRel=esriSpatialRelIntersects&where=state%20%3D%20%27DD%27")
-
-
-"""
-
-https://arc.indiawris.gov.in/server/rest/services/Common/Administrative_SOI/MapServer/1?f=json
-"""
-
 
 
 
@@ -16,8 +8,12 @@ response = requests.post("https://arc.indiawris.gov.in/server/rest/services/Comm
 
 states = response["fields"][2]['domain']['codedValues']
 
-for ky in states :
-    print(ky['name'], ky['code'])
+for key in states :
+    response = requests.post("https://arc.indiawris.gov.in/server/rest/services/Common/Administrative_SOI/MapServer/1/query?f=json&orderByFields=district&outFields=*&spatialRel=esriSpatialRelIntersects&where=state%20%3D%20%27{}%27".format(key['code'])).json()
+    print(key['name'], key['code'], len(str(response).encode('utf-8')))
+    if (key['code'] == 'DD'):
+        # print(response)
+        print (10 )
 
 
 # """
